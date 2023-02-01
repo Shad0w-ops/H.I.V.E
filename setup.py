194616402e8a2735931b47bf7242d1126861b65d
Shad0w-ops <43708460+Shad0w-ops@users.noreply.github.com>
2023-02-01 05:28:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,4 @@ import os
 os.system("sudo apt install libimage-exiftool-perl")
 os.system("pip3 install -r requirements.txt")
 os.system("bash Extras/kali-anonsurf/installer.sh")
+os.system("sudo apt install macchanger")
