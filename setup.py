90e5d14f33a36040d2cf3cfc995a77ac1f4a8f07
EliteGreyIT <EliteGreyIT67@users.noreply.github.com>
2023-10-13 20:12:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,19 +10,20 @@ os.system("bash Extras/kali-anonsurf/installer.sh")
 os.system("sudo apt install macchanger")
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(name='intelx',
-      version='0.5',
-      description='IntelX is a Python command-line utility and API wrapper for intelx.io, made to perform any kind of open-source intelligence.',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Kleissner Investments s.r.o./ Dominik Penner',
-      author_email='info@intelx.io',
-      keywords='intelx intelligence x intelx.io _IntelligenceX IntelligenceX _intelx',
-      url='https://github.com/IntelligenceX/SDK/Python',
-      packages=['.'],
-      scripts=['cli/intelx.py'],
-      install_requires=['requests', 'pygments', 'termcolor', 'tabulate']
-     )
+setup(
+    name="intelx",
+    version="0.5",
+    description="IntelX is a Python command-line utility and API wrapper for intelx.io, made to perform any kind of open-source intelligence.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Kleissner Investments s.r.o./ Dominik Penner",
+    author_email="info@intelx.io",
+    keywords="intelx intelligence x intelx.io _IntelligenceX IntelligenceX _intelx",
+    url="https://github.com/IntelligenceX/SDK/Python",
+    packages=["."],
+    scripts=["cli/intelx.py"],
+    install_requires=["requests", "pygments", "termcolor", "tabulate"],
+)
